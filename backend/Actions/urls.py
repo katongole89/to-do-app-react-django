7046e02c66eb4539273b6e3b9cdb75bea9e45f78
katongole89 <katongolerichard089@gmail.com>
2021-04-23 18:29:39 +0300

    creating the list todo view

--- backend/Actions/urls.py
+++ backend/Actions/urls.py
@@ -3,6 +3,6 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.index.as_view(), name = 'index'),
+    path('to-dos/', views.listToDos.as_view(), name = 'listToDos'),
     ]
 urlpatterns= format_suffix_patterns(urlpatterns)
--- backend/Actions/views.py
+++ backend/Actions/views.py
@@ -5,11 +5,21 @@ from Accounts.models import Users
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status as alt_status
+from rest_framework.permissions import IsAuthenticated
 #from .services import make_auth_response_data
-
+from rest_framework.authtoken.models import Token
+from .models import ToDo
 # Create your views here.
-class index(APIView):
+class listToDos(APIView):
+    permission_classes = (IsAuthenticated,)   
     def get(self,request):
+        authToken = request.META.get('HTTP_AUTHORIZATION')
+        authToken = authToken.replace("Token ", "")
+        person = Token.objects.get(key = authToken ).user
+
+        #query data
+        queryToDos = ToDo.objects.filter(user = person)
+
         data ={
             'detail':'setup'
         }
