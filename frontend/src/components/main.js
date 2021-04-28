710cc7ffdb4bc6887f7958b98dffb81daeb7add6
katongole89 <katongolerichard089@gmail.com>
2021-04-28 16:41:02 +0300

    check user if logged in

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,16 +1,27 @@
 import React, {useEffect} from "react"
 import {useCookies} from 'react-cookie'
+import {useHistory} from 'react-router-dom'
 
 function Main(){
 
-
     const[User, setUser] = useCookies(['currentUser'])
-    console.log(User);
+    let history = useHistory()
+    //check log in status
+    if(!User.currentUser){
+        console.log('U NEED TO LOGIN')
+        history.push('/login')
+        return(<div></div>)
+    }else{
+        //update cookie to new time
+        setUser('currentUser', User.currentUser, {
+            maxAge:3600
+        })
+    }
     return(
         <div>
             <h1>home</h1>
             <div>
-                first name:{}
+                first name:{User.currentUser.first_name}
                 last name: {}
             </div>
         </div>
